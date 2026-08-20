5961541989aba7a613083c3f25f934766880ff59
Pedro Philipe <PEDROPHILIPE2019@GMAIL.COM>
2026-08-19 21:00:46 -0300

    Adicionar script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("JavaScript conectado!");
+console.log("Document");
+
+const titulo = document.querySelector("h1");
+console.log(titulo);
+
+const hero = document.querySelector("#hero");
+console.log(hero);
+
+const linkSocial = document.querySelector(".link-social");
+console.log(linkSocial);
+console.log(linkSocial.textContent);
+
+const título = document.querySelector("h1");
+título.textContent = " Pedro Philipe | Desenvolvedor Web";
+
+const herói = document.querySelector("#hero");
+const apresentação = herói.querySelector("p");
+apresentação.textContent =
+ "Olá, meu nome é Pedro Philipe e eu estou começando minha jornada no desenvolvimento web.";
+
+ document.getElementById("hero");
+ console.log(herói);
