7b07ee28c229ecb6f713805e2338156cfb1c28e6
Pedro Philipe <PEDROPHILIPE2019@GMAIL.COM>
2026-08-25 20:40:03 -0300

    buttom
    Added event listener to button for dynamic content.

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-console.log("JavaScript conectado!");
-console.log("Document");
+const SaibaMais = document.getElementById("botao-sobre");
 
-const titulo = document.querySelector("h1");
-console.log(titulo);
+const Novotexto = document.getElementById("Novo-texto");
 
-const hero = document.querySelector("#hero");
-console.log(hero);
+SaibaMais.addEventListener("click",function(){
+    const novoParagrafo = document.createElement("p");
 
-const linkSocial = document.querySelector(".link-social");
-console.log(linkSocial);
-console.log(linkSocial.textContent);
+    novoParagrafo.textContent =
+    "Me faça um pix.";
 
-const título = document.querySelector("h1");
-título.textContent = " Pedro Philipe | Desenvolvedor Web";
-
-const herói = document.querySelector("#hero");
-const apresentação = herói.querySelector("p");
-apresentação.textContent =
- "Olá, meu nome é Pedro Philipe e eu estou começando minha jornada no desenvolvimento web.";
-
- document.getElementById("hero");
- console.log(herói);
+    Novotexto.appendChild(novoParagrafo);
+})
