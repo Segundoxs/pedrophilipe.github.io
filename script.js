4a6fefa43ae222f6f25d3cce0b5b2fce4870d35e
Pedro Philipe <PEDROPHILIPE2019@GMAIL.COM>
2026-08-26 21:06:17 -0300

    atualização

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ SaibaMais.addEventListener("click",function(){
 
     Novotexto.appendChild(novoParagrafo);
 })
+const formulario = document.querySelector("form");
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+
+formulario.addEventListener("submit",function(event){
+
+    console.log("Formulário enviado!");
+});
+
+formulario.addEventListener("submit",function(event){
+
+    event.preventDefault();
+    
+    console.log("Nome:",nome.value);
+    console.log("E-mail:",email.value);
+});
+const mensagemSucesso = 
+        document.getElementById("mensagem-sucesso");
+mensagemSucesso.textContent = "Mensagem enviada com sucesso!";
+
+const contador = document.getElementById("contador");
+
+mensagem.addEventListener("input",function(){
+    contador.textContent = mensagem.value.length + "caracteres";
+});
+
+let mostrandoMais = false;
+
+SaibaMais.addEventListener("click", function(){
+    if (mostrandoMais === false) {
+        const novoParagrafo = document.createElement("p");
+        novoParagrafo.textContent = 
+        "Me faça um pix.";
+
+        Novotexto.appendChild(novoParagrafo);
+
+        SaibaMais.textContent = "Ver menos";
+        mostrandoMais = true;
+
+    } else{
+        Novotexto.innerHTML = "";
+        SaibaMais.textContent = "Ver mais";
+        mostrandoMais = false;
+    }
+});
